aded1a3c0e7da421b41b9a554f3abe2374c931e1
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2022-03-26 01:43:59 +0500

    work

---
+++ app.js
@@ -0,0 +1,91 @@
+
+/////javascript assigment 1////
+    // alert////
+             //Q.1
+// alert("السلام عليكم")
+            // Q.2
+// alert("Error! enter your valid password.")
+             //Q.3
+// alert("huzaifa sheikh...\nhappy coding.")
+            //Q.4
+// var Q.5 =prompt("enter Message")
+// console.log(Q5)
+            // Q.6//
+// Make use of alerts in your new/existing HTML & CSS 
+            // Q.7//
+            // script tags before body after and head.. done
+// chapter 2
+ // VARIABLES FOR STRINGS//
+            // 1.//
+ // var username =("huzaifa");
+      // alert (username);
+           // 2.//    
+// var myName=("huzaifasheikh");
+ // alert(myName);
+       // 3.//
+ // var message =("Hello world");
+ // alert(message)
+      //4.//
+  // var Name =("huzaifa");
+  // alert(Name);
+ // var fatherName =("Nadeem");
+ // alert(fatherName);
+// var age =("20");
+// alert(age);
+     //5.//
+     // var Q5=("PIZZAPIZZ\nPIZ\nPI\nP")   
+ // alert(Q5)
+    //6.//
+// var email=("my email address is "+"huzaifanadeem621@gmail")
+// alert (email)
+//     // 7.//
+// var book=("A smarter way to learn java script")
+// alert(book)
+//8.//
+// document.write("Yah! i can write html through javascript")
+// // Q.9///
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// document.write(Name)
+            //chapter # 3//
+                //1.//
+//  var age=(20)
+//  alert (age)   
+                //2.//
+// var visted=("you have visted this site 14 times")
+// alert(visted)
+            //3.//
+// var birthYear =("my birth year is 2002 age is 20 ");
+// document.write(birthYear);
+            //4.//
+// var storeSite =("<b>huzaifa</b> ordered <b>5 T-shirt</b>(s) on XYZ Clothing store")
+// document.write(storeSite)
+            //chapter#4//
+           // 1.//
+//         //  Declare 5 legal & 5 illegal variable names
+// var 1name=(huzaifa)
+// var #name=(huzaifa)
+// var ^name=(huzaifa)
+// var name& =(huzaifa)
+// var na*me =(huzaifa)
+// // legal//
+// var name=(huzaifa)
+// var name=(huzaifa)
+// var myName=(huzaifa)
+// var MyName=(huzaifa)
+// var MyName1=(huzaifa)
+// var M_Name=(huzaifa)
+// var $M_Name=(huzaifa)  
+              //2.//
+            //(a)//
+// var heading =("<b>“Rules for naming JS variables”</b>")
+                     //(b)
+//Variable names can only contain $_// 
+                     //(c)
+//  documnet.write("Variables must begin with a smallletter,capitalletter or $.)
+// document.write("Variable names can only contain _, $, ")
+                      //(d)
+// document.write("variable names are camelcase")
+                      //(e)
+         // Variable names should not be JS keywords")//
+        // document.write("Variable names should not be start numerical")
+
