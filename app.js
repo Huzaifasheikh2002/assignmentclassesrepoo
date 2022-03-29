e0c0535e6d6a6ab384ce093da4194cc64f792235
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2022-03-30 02:11:45 +0500

    workkk

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 
 /////javascript assigment 1////
-    // alert////
-             //Q.1
+// alert////
+//Q.1
 // alert("السلام عليكم")
-            // Q.2
+// Q.2
 // alert("Error! enter your valid password.")
-             //Q.3
+//Q.3
 // alert("huzaifa sheikh...\nhappy coding.")
-            //Q.4
+//Q.4
 // var Q.5 =prompt("enter Message")
 // console.log(Q5)
-            // Q.6//
+// Q.6//
 // Make use of alerts in your new/existing HTML & CSS 
-            // Q.7//
-            // script tags before body after and head.. done
+// Q.7//
+// script tags before body after and head.. done
 // chapter 2
- // VARIABLES FOR STRINGS//
-            // 1.//
- // var username =("huzaifa");
-      // alert (username);
-           // 2.//    
+// VARIABLES FOR STRINGS//
+// 1.//
+// var username =("huzaifa");
+// alert (username);
+// 2.//    
 // var myName=("huzaifasheikh");
- // alert(myName);
-       // 3.//
- // var message =("Hello world");
- // alert(message)
-      //4.//
-  // var Name =("huzaifa");
-  // alert(Name);
- // var fatherName =("Nadeem");
- // alert(fatherName);
+// alert(myName);
+// 3.//
+// var message =("Hello world");
+// alert(message)
+//4.//
+// var Name =("huzaifa");
+// alert(Name);
+// var fatherName =("Nadeem");
+// alert(fatherName);
 // var age =("20");
 // alert(age);
-     //5.//
-     // var Q5=("PIZZAPIZZ\nPIZ\nPI\nP")   
- // alert(Q5)
-    //6.//
+//5.//
+// var Q5=("PIZZAPIZZ\nPIZ\nPI\nP")   
+// alert(Q5)
+//6.//
 // var email=("my email address is "+"huzaifanadeem621@gmail")
 // alert (email)
 //     // 7.//
@@ -46,21 +46,21 @@
 // // Q.9///
 // alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
 // document.write(Name)
-            //chapter # 3//
-                //1.//
+//chapter # 3//
+//1.//
 //  var age=(20)
 //  alert (age)   
-                //2.//
+//2.//
 // var visted=("you have visted this site 14 times")
 // alert(visted)
-            //3.//
+//3.//
 // var birthYear =("my birth year is 2002 age is 20 ");
 // document.write(birthYear);
-            //4.//
+//4.//
 // var storeSite =("<b>huzaifa</b> ordered <b>5 T-shirt</b>(s) on XYZ Clothing store")
 // document.write(storeSite)
-            //chapter#4//
-           // 1.//
+                        //chapter#4//
+                        // 1.//
 //         //  Declare 5 legal & 5 illegal variable names
 // var 1name=(huzaifa)
 // var #name=(huzaifa)
@@ -75,17 +75,158 @@
 // var MyName1=(huzaifa)
 // var M_Name=(huzaifa)
 // var $M_Name=(huzaifa)  
-              //2.//
-            //(a)//
+//2.//
+//(a)//
 // var heading =("<b>“Rules for naming JS variables”</b>")
-                     //(b)
+//(b)
 //Variable names can only contain $_// 
-                     //(c)
+//(c)
 //  documnet.write("Variables must begin with a smallletter,capitalletter or $.)
 // document.write("Variable names can only contain _, $, ")
-                      //(d)
+//(d)
 // document.write("variable names are camelcase")
-                      //(e)
-         // Variable names should not be JS keywords")//
-        // document.write("Variable names should not be start numerical")
+//(e)
+// Variable names should not be JS keywords")//
+// document.write("Variable names should not be start numerical")
+// chapter #5//
+                                //1.//
+
+// alert(add)
+// var num1 = prompt("Enter number 1");
+// var num2 = prompt("Enter number 2");
+// var input = prompt("Enter Operator");
+// if(input === "+"){
+// console.log(num1 + num2)
+// }
+                        //2.//
+        //multiply//  //(  *   )//
+
+// var num1 =prompt("enter num1")
+// alert ("*")
+// var num2 =prompt("enter num2")
+
+// console.log (num1 *num2)
+                //(  -  ) minus//
+// var num1 =prompt("enter num1")
+// alert ("-")
+// var num2 =prompt("enter num2")
+
+// console.log (num1 -num2)
+
+                //divide (  /  )//
+// var num1 =prompt("enter num1")
+// alert ("/")
+// var num2 =prompt("enter num2")
+
+// console.log (num1 /num2)
+
+
+//////chapter 5 quetsion 4////
+
+// var movieTicket=(600);
+// var buying=(5);
+// var totalCost=(movieTicket *buying);
+// document.write("Total cost to buy a ticket is ",totalCost, "PKR")
+                  ///////
+
+
+                    /////question 5.///
+// var table = ("Table of 4<br /> 4*1=4<br />4*1=8 <br />4*1=12<br />4*1=16<br />4*1=20<br />4*1=24<br />4*1=28<br />4*1=32<br />4*1=36<br />4*1=40")
+
+
+
+// document.write(table)
+
+                // question 7/
+
+// document.write("<h1>shopping cart</h1>")
+// var priceOfitem=(650);
+// document.write("price of item 1 = ",priceOfitem,"<br />");
+// var quanityOfItem1=(3);
+// document.write("quanity of Item 1 is ",quanityOfItem1,"<br />");
+// var priceOfItem2=(100)
+// document.write("price of item 2 is ",priceOfItem2,"<br />");
+// var quanityOfItem2=(7)
+// document.write("quanity Of Item 2 is ",quanityOfItem2,"<br />");
+// var shipingCharge= 100;
+// document.write((priceOfitem*quanityOfItem1)+(priceOfItem2*quanityOfItem2)+100);
+
+       
+          // 13. The Lifetime Supply Calculator: Ever wonder how
+                       
+// document.write("<h1>The Lifetime Supply Calculator</h1>");
+// var snack=("lays")
+// document.write("fav snack :",snack,"<br />");
+// var age=(15)
+// document.write("Current age: ",age,"<br />");
+// var maxAge=(65)
+// document.write("Max age: ", maxAge,"<br />");
+// var perDay=(3);
+// var perDayEat=(45)
+// document.write("you will need ",perDay*maxAge-perDayEat," to last until the ripe old age of 65");
+                                        ///////
+
+// var num1 =(10)
+// var num2 =(20)
+// // var num1=prompt("enter no 1")
+// // var num2=prompt("enter no 2")
+// // var num2=prompt()
+// alert (num1)
+// alert (num2)
+// var result = num1 + num2;
+// document.write(result);
+// // console.log(result)
+
+
+
+                        ////chapter9/////
+                                ///1//
+// var city =prompt("Enter your city name")
+// if (city==="karachi"){ 
+// document.write("<h1>Welcome to city light</h1>")
+// }
+// else{ ("enter your city name")
+// document.write("<h1>Enter your city name</h1>")
+// }
+                        ////2////
+// var gender=prompt ("ENTER YOUR GENDER")
+// if(gender==="male"){
+// document.write("hello boss")
+// }
+// if(gender==="female"){
+// document.write("hello madam")
+//  }
+
+// else{ ("enter your gender")
+// document.write("enter your gender")
+// }
+
+
+                        //////last chapter12,13// ////
+
+                                // question 5.
+
+// var pass=(4321)
+// var password = prompt("enterpassword")
+// if (pass ===4321){ 
+// console.log("correct")
+
+// }
+// else{ 
+//    console.log("incorrect") 
+// }
+////6///
+
+// var greeting;
+// var hour = (13);
+// var input= prompt("enter time")
+// if (hour === 13) {
+// console.log=("Good day");
+// }
+// else{ 
+// console.log=("Good evening");
+// }
+
+
+
 
